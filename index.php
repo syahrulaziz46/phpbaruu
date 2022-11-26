c97590d701a2b17e6094602cf8d22446d1bcb26e
syahrulaziz46 <100403881+syahrulaziz46@users.noreply.github.com>
2022-11-26 23:21:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>KABUPATEN PONOROGO</title>
+</head>
+<body>
+    <!--navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+        <div class="container">
+          <a href="#" class="navbar-brand">KABUPATEN PONOROGO</a>
+          <button class="navbar-toggler" type="button"data-bs-toggle="collapse"data-bs-target="#navmenu" ><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item"><a href="#home" class="nav-link">HOME</a></li>
+              <li class="nav-item"><a href="#SEJARAH" class="nav-link">SEJARAH</a> </li>
+              <li class="nav-item"><a href="#KESENIAN" class="nav-link">KESENIAN</a>
+              <li class="nav-item"><a href="#WISATA" class="nav-link">WISATA</a></li>
+            </ul>
+          </div>
+      </nav>
+    </body>
+     <!-- Home -->
+    <section id="home"
+     class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start" >
+     <div class="container">
+       <div class="d-sm-flex align-items-center justify-content-between">
+         <div>
+           <h1>SELAMAT <span class="text-warning"> DATANG </span></h1>
+           <h1>DI KOTA <span class="text-warning"> REOG </span></h1>
+         </div>
+       </div>
+     </div>
+   </section>
+   <!-- Learn Sections -->
+   <section id="SEJARAH" class="p-5">
+    <div class="container">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-md">
+          <img src="kabupatenpo.jpg" class="img-fluid" alt="" />
+        </div>
+        <div class="col-md p-5">
+          <h2>SEJARAH</h2>
+          <p>
+            Kabupaten Ponorogo berdiri pada tanggal 11 Agustus 1496 Masehi, tanggal inilah yang kemudian di tetapkan sebagai hari jadi kota Ponorogo. Penetapan tanggal ini merupakan kajian mendalam atas dasar bukti peninggalan benda-benda purbakala di daerah Ponorogo dan sekitarnya, juga mengacu pada buku Hand book of Oriental History, sehingga dapat ditemukan hari wisuda Bathoro Katong sebagai Adipati Kadipaten Ponorogo. Bathoro Katong adalah pendiri Kadipaten Ponorogo yang selanjutnya
+             berkembang menjadi Kabupaten Ponorogo
+          </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+   <!-- Learn Sections -->
+   <section id="KESENIAN" class="p-5">
+    <div class="container">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-md">
+          <img src="REOG.webp" class="img-fluid" alt="" />
+        </div>
+        <div class="col-md p-5">
+          <h2>KESENIAN</h2>
+          <p>
+            Sejarah Singkat Reog Ponorogo telah menjadi kebanggaan masyarakat Kabupaten Ponorogo sejak jaman dahulu hingga sekarang ini. Reog Ponorogo sudah berumur lebih dari 2 abad. Hal itu dapat dilihat dari naskah Kanjeng Gusti Pangeran Adipati Anom Amengkunagara III Ing Surakarta, Pupuh di Serat Centini. Namun, menurut menurut legenda, keberadaan seni reog sudah ada sejak masa Kerajaan Kediri abad XI. Hal ini terkait cerita prosesi lamaran Prabu Kelanasewandana dari Kerajaan Wengker kepada Dewi Sanggalangit dari Kerajaan Kediri. Legenda ini dianggap sebagai versi sejarah tertua dari reog. Hingga saat ini cerita legenda Prabu Kelanasewandana tersebut masih dijadikan sebagai salah satu alur cerita dari seni pertunjukan reog di Ponorogo. 
+            Alur cerita ini kemudian dikenal sebagi pertunjukan seni reog versi legenda Bantarangin.
+          </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Learn Sections -->
+  <section id="WISATA" class="p-5">
+    <div class="container">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-md">
+          <img src="telaga ngebel ponorogo.jpg" class="img-fluid" alt="" />
+        </div>
+        <div class="col-md p-5">
+          <h2>WISATA</h2>
+          <p>
+            Telaga Ngebel adalah sebuah danau alami yang terletak di Kecamatan Ngebel, 23 kilometer dari pusat kota Ponorogo, atau sekitar 40 menit perjalanan. Dengan keliling telaga mencapai 5 kilometer, luas Telaga Ngebel mencapai 150 hektar. Jika anda mengenal Telaga Sarangan di Kabupaten Magetan, Telaga Ngebel ini lima kali lipat lebih luas dibandingkan Telaga Sarangan. Kawasan Telaga Ngebel berada pada ketinggian 734 meter diatas permukaan laut (mdpl). Hal ini membuat kawasan Telaga Ngebel selalu diselimuti 
+            udara yang sejuk dan dingin pada kisaran 20° celcius. Anda dapat menikmati pemandangan telaga yang dikelilingi oleh hutan pinus dengan menyusurinya menggunakan bus air, perahu santai, maupun speed boat. Terdapat beberapa objek wisata lain yang dapat dikunjungi pula jika anda berwisata ke Telaga Ngebel, antara lain Air Terjun Toyomarto, Air Terjun Selorejo, Air Tiga Rasa, dan juga pemandian air panas Tirto Husodo.
+          </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--FORM-->
+ <section>
+    <body style="text-align: center">
+        <h1></h1>
+        <form  action="welcome.php" method="post">
+        Nama: <br>
+        <input type="text" name="nama"><br>
+        Email: <br>
+        <input type="text" name="email"><br>
+        kritik: <br>
+        <input type="text" name="kritik"><br>>
+        <input type = "submit">
+        </form>
+    </body>
+    <div id="status"></div>
+  </section>
+  <!------footer start--------->
+	<footer>
+		<p>PONOROGO HEBAT BERMARTABAT</p>
+		<p>For more information, subscribe to my channel ponorogo :</p>
+		<div class="social">
+			<a href="https://www.facebook.com/pngkab"><i class="fab fa-facebook-f"></i></a>
+			<a href="https://instagram.com/ponorogokab?igshid=YmMyMTA2M2Y="><i class="fab fa-instagram"></i></a>
+			<a href="https://twitter.com/PonorogoKab?t=zRzzmEjck8_xyX1PfwkeVw&s=08"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.youtube.com/channel/UCGu5NLgGEbI4_qKqb-waLdw"><i class="fab fa-youtube"></i></a>
+		</div>
+		<p class="end">CopyRight By Admin Ponorogo</p>
+	</footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<body>
+    
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        Welcome <?php echo $_POST["nama"];?><br>
+        Your Email Addres is: <?php echo $_POST["email"];
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="mydb";
+    $nama=$_POST["nama"];
+    $email=$_POST["email"];
+    $kritik=$_POST["kritik"];
+    
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed:".$conn->connect_error);
+    }
+    
+    $sql = "INSERT INTO myguests (nama, email, kritik)
+    VALUES ('$nama', '$email', '$kritik');";
+    
+    if($conn->query($sql) === TRUE){
+        echo "New records created succesfully";
+    } else {
+        echo "Error: ". $sql . "<br>" . $conn->error;
+    }
+    
+    $conn = new mysqli ($servername, $username, $password, $dbname);
+    $sql=  "SELECT nama, email, kritik FROM myguets";
+   
+    ?>
+    </body>
+</html>
